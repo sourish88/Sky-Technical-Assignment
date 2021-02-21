2703e5a98beaf9333a36897c2e73bf9ee779eb3e
sourish88 <sourish88@gmail.com>
2021-02-21 19:35:56 +0000

    show hostname

--- app/tasks/apache.sh
+++ app/tasks/apache.sh
@@ -4,8 +4,9 @@ set -e
 echo '---- install Apache'
 
 DEBIAN_FRONTEND=noninteractive apt-get -y update
-DEBIAN_FRONTEND=noninteractive apt-get -y install apache2
-
+DEBIAN_FRONTEND=noninteractive apt-get -y install apache2 stress
+instance_hostname=$(curl http://169.254.169.254/latest/meta-data/hostname)
 cat > /var/www/html/index.html <<HERE
-Hello There, this is Sourish Banerjee !
+Hello There,
+This is node:  $instance_hostname
 HERE
