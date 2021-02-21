4677d852805a9f10b7f74287af4075a2fc35efeb
sourish88 <sourish88@gmail.com>
2021-02-21 19:51:28 +0000

    update index.html

--- app/tasks/apache.sh
+++ app/tasks/apache.sh
@@ -4,8 +4,8 @@ set -e
 echo '---- install Apache'
 
 DEBIAN_FRONTEND=noninteractive apt-get -y update
-DEBIAN_FRONTEND=noninteractive apt-get -y install apache2 stress
-instance_hostname=$(curl http://169.254.169.254/latest/meta-data/hostname)
+DEBIAN_FRONTEND=noninteractive apt-get -y install apache2
+export instance_hostname=$(curl http://169.254.169.254/latest/meta-data/hostname)
 cat > /var/www/html/index.html <<HERE
 Hello There,
 This is node:  $instance_hostname
