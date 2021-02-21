60d86d09d2d261ce9e98ea12c7d8b26348a838d1
sourish88 <sourish88@gmail.com>
2021-02-21 13:35:24 +0000

    fix test

--- aws-security/controls/example.rb
+++ aws-security/controls/example.rb
@@ -3,7 +3,6 @@ content = inspec.profile.file("output.json")
 params = JSON.parse(content)
 
 vpc_id = params['vpc_id']['value']
-public_subnets = params['public_subnets']['value']
 web_sec_grp_id = params['web_sec_grp_id']['value']
 web_elb_sec_grp_id = params['web_elb_sec_grp_id']['value']
 web_asg_id = params['web_asg_id']['value']
@@ -11,19 +10,6 @@ web_elb_id = params['web_elb_id']['value']
 
 describe aws_vpc(vpc_id) do
   its('state') { should eq 'available' }
-end
-
-control 'aws-subnets-loop' do
-
-  impact 1.0
-  title 'Loop across AWS VPC Subnets resource for detail.'
-  
-  aws_subnets.where public_subnets.each do |subnet|
-      describe aws_subnet(subnet) do
-          it                      { should be_available }
-          its ('vpc_id')          { should eq aws_vpc_id }
-      end
-  end
 end
 
 describe aws_security_group(web_sec_grp_id) do
@@ -34,9 +20,9 @@ describe aws_security_group(web_elb_sec_grp_id) do
   it { should exist }
 end
 
-# describe aws_auto_scaling_group(web_asg_id) do
-#   its('state') { should eq 'available' }
-# end
+describe aws_auto_scaling_group(web_asg_id) do
+  its('state') { should eq 'available' }
+end
 
 describe aws_elb(web_elb_id) do
   it { should exist }
